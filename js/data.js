351cd5f440f0893f6989adbcc46c13944488e986
andrew-samuel-dickinson <andrew.samuel.dickinson@gmail.com>
2020-08-18 14:23:59 +0100

    Updated images

--- js/data.js
+++ js/data.js
@@ -1,27 +1,27 @@
 // TODO move this to JSON?
 myQaData = [
     {"Q" : "What is 5 x 11", "A" : "55"},
-    {"Q" : "What is 8 x 11", "A" : "88"},
-    {"Q" : "What is 11 x 8", "A" : "88"},
-    {"Q" : "What is 8 x 9", "A" : "72"},
-    {"Q" : "What is 9 x 9", "A" : "81"},
-    {"Q" : "What is 5 x 5", "A" : "25"},
-    {"Q" : "What is 6 x 6", "A" : "36"},
-    {"Q" : "What is 7 x 7", "A" : "49"},
-    {"Q" : "What is the missing number: 9, 18, 27, ??, 45", "A" : "36"},
-    {"Q" : "What is the missing number: 1, 4, 7, ??, 13, 16", "A" : "10"},
-    {"Q" : "What is the missing number: 54, 45, 36, ??, 18, 9", "A" : "27"},
-    {"Q" : "What is the missing number: 2, 4, 8, ??", "A" : "16"},
-    {"Q" : "What is 13 x 10", "A" : "130"},
-    {"Q" : "What is 13 x 11", "A" : "143"},
-    {"Q" : "I am 4 years older than my brother. I am also twice his age. How old am I?", "A" : "8"},
+    // {"Q" : "What is 8 x 11", "A" : "88"},
+    // {"Q" : "What is 11 x 8", "A" : "88"},
+    // {"Q" : "What is 8 x 9", "A" : "72"},
+    // {"Q" : "What is 9 x 9", "A" : "81"},
+    // {"Q" : "What is 5 x 5", "A" : "25"},
+    // {"Q" : "What is 6 x 6", "A" : "36"},
+    // {"Q" : "What is 7 x 7", "A" : "49"},
+    // {"Q" : "What is the missing number: 9, 18, 27, ??, 45", "A" : "36"},
+    // {"Q" : "What is the missing number: 1, 4, 7, ??, 13, 16", "A" : "10"},
+    // {"Q" : "What is the missing number: 54, 45, 36, ??, 18, 9", "A" : "27"},
+    // {"Q" : "What is the missing number: 2, 4, 8, ??", "A" : "16"},
+    // {"Q" : "What is 13 x 10", "A" : "130"},
+    // {"Q" : "What is 13 x 11", "A" : "143"},
+    // {"Q" : "I am 4 years older than my brother. I am also twice his age. How old am I?", "A" : "8"},
 ];
 
 images = 
 [
     "https://i.pinimg.com/236x/70/dd/52/70dd5208032fdcac975727e4d1484c18--funny-stuff-so-funny.jpg",
     "https://i.insider.com/5c79a8cfeb3ce837863155f5?width=750&format=jpeg&auto=webp",
-    "https://filmdaily.co/wp-content/uploads/2020/04/funny-animals-lede.jpg", 
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS6YDBxxgiWarH-2u2chH1riSbd5aHnG0tJBw&usqp=CAU", 
     "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRphb761m4liOHpIMdxOVkPrP7IEq69JQib1g&usqp=CAU"
 ];
 
