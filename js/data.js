2a481c4139fc5f6fdbe7513e5e8030e3d0d55ba4
andrew-samuel-dickinson <andrew.samuel.dickinson@gmail.com>
2020-08-19 10:10:19 +0100

    Updated questions and links

--- js/data.js
+++ js/data.js
@@ -1,31 +1,34 @@
 // TODO move this to JSON?
 myQaData = [
-    {"Q" : "What is 5 x 11", "A" : "55"},
-    {"Q" : "What is 8 x 11", "A" : "88"},
-    {"Q" : "What is 11 x 8", "A" : "88"},
-    {"Q" : "What is 8 x 9", "A" : "72"},
-    {"Q" : "What is 9 x 9", "A" : "81"},
-    {"Q" : "What is 5 x 5", "A" : "25"},
-    {"Q" : "What is 6 x 6", "A" : "36"},
-    {"Q" : "What is 7 x 7", "A" : "49"},
-    {"Q" : "What is the missing number: 9, 18, 27, ??, 45", "A" : "36"},
-    {"Q" : "What is the missing number: 1, 4, 7, ??, 13, 16", "A" : "10"},
-    {"Q" : "What is the missing number: 54, 45, 36, ??, 18, 9", "A" : "27"},
-    {"Q" : "What is the missing number: 2, 4, 8, ??", "A" : "16"},
-    {"Q" : "What is 13 x 10", "A" : "130"},
-    {"Q" : "What is 13 x 11", "A" : "143"},
-    {"Q" : "I am 4 years older than my brother. I am also twice his age. How old am I?", "A" : "8"},
+    {"Q" : "5 x ?? = 55", "A" : "11"},    
+    {"Q" : "55 &#247; 5 = ", "A" : "11"},
+    {"Q" : "?? x 11 = 88", "A" : "8"},
+    {"Q" : "88 &#247; 11 = ", "A" : "8"},
+    {"Q" : "?? x 2 = 22", "A" : "11"},
+    {"Q" : "22 &#247; 2 = ", "A" : "11"},
+    {"Q" : "9 &#247; 3 = ", "A" : "3"},
+    {"Q" : "15 &#247; 3 = ", "A" : "5"},
+    {"Q" : "100 &#247; 2 = ", "A" : "50"},
+    {"Q" : "16 &#247; 4 = ", "A" : "4"},
+    {"Q" : "45 &#247; 5 = ", "A" : "9"},
+    
+    {"Q" : "What is the missing number: 11, 22, 33, ??, ", "A" : "44"},
+    {"Q" : "What is the missing number: 2, 6, 10, ??, 18", "A" : "14"},
+    {"Q" : "What is the missing number: 64, 32, 16, ??, 4", "A" : "8"},
+    {"Q" : "What is the missing number: 1, 1, 2, 3, 5, 8, ??", "A" : "13"},
+    
+    {"Q" : "I am 6 years older than my brother. I am also three times his age. How old am I?", "A" : "9"},
 ];
 
 images = 
 [
-    "https://i.pinimg.com/236x/70/dd/52/70dd5208032fdcac975727e4d1484c18--funny-stuff-so-funny.jpg",
+    "https://i0.wp.com/bestlifeonline.com/wp-content/uploads/2018/04/Animal-jokes-funny-sheep.jpg?resize=1024%2C683&ssl=1",
     "https://i.insider.com/5c79a8cfeb3ce837863155f5?width=750&format=jpeg&auto=webp",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS6YDBxxgiWarH-2u2chH1riSbd5aHnG0tJBw&usqp=CAU", 
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRphb761m4liOHpIMdxOVkPrP7IEq69JQib1g&usqp=CAU"
+    "https://piximus.net/media2/58686/funny-animals-61-2.jpg", 
+    "https://www.abc.net.au/cm/rimage/11514316-4x3-xlarge.jpg?v=3"
 ];
 
-video_id = "RW61RQZojMQ";
+video_id = "pHZneOidj9A";
 video = "https://www.youtube.com/embed/" + video_id + "?autoplay=1";
 
 default_image = "http://placehold.it/800x500"
