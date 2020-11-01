6454eb272699b6751f693b9373e831f3a3dd85d0
andrew-samuel-dickinson <andrew.samuel.dickinson@gmail.com>
2020-11-01 12:57:11 +0000

    Question update.

--- js/pictures.js
+++ js/pictures.js
@@ -1,7 +1,7 @@
 images = 
 [
-    "https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2013/12/17/1387283640669/A-red-squirrel-leaps-betw-015.jpg?width=300&quality=85&auto=format&fit=max&s=97f937cbf7e9b62b1e93da004cee4108",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Suricata.jpg/200px-Suricata.jpg",
-    "https://kids.nationalgeographic.com/content/dam/kids/photos/articles/Other-Explore-Photos/R-Z/Wacky-Weekend/Funny-Animal-Faces/ww-funny-animal-faces-lemur.adapt.1900.1.jpg",
-    "https://us.123rf.com/450wm/sonsedskaya/sonsedskaya1906/sonsedskaya190600035/126327060-portrait-of-a-funny-raccoon-in-red-sunglasses-showing-a-rock-gesture.jpg?ver=6"
+    "https://static.standard.co.uk/s3fs-public/thumbnails/image/2017/12/15/11/comedywildlife1512a-0.jpg?width=1000&height=614&fit=bounds&format=pjpg&auto=webp&quality=70&crop=16:9,offset-y0.5",
+    "https://i.pinimg.com/originals/11/30/76/113076ae22b67cd7a73f49ab42e94b8c.png",
+    "https://images-na.ssl-images-amazon.com/images/I/41abcm7YWOL._AC_.jpg",
+    "https://www.rd.com/wp-content/uploads/2018/12/50-Funny-Animal-Pictures-That-You-Need-In-Your-Life-10.jpg"
 ];
