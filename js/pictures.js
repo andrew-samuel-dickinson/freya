635ef3e8732af3de4805275416aab09845f766bf
andrew-samuel-dickinson <andrew.samuel.dickinson@gmail.com>
2020-11-08 15:21:11 +0000

    Updated questions for Freya.

--- js/pictures.js
+++ js/pictures.js
@@ -1,7 +1,7 @@
 images = 
 [
-    "https://static.standard.co.uk/s3fs-public/thumbnails/image/2017/12/15/11/comedywildlife1512a-0.jpg?width=1000&height=614&fit=bounds&format=pjpg&auto=webp&quality=70&crop=16:9,offset-y0.5",
-    "https://i.pinimg.com/originals/11/30/76/113076ae22b67cd7a73f49ab42e94b8c.png",
-    "https://images-na.ssl-images-amazon.com/images/I/41abcm7YWOL._AC_.jpg",
-    "https://www.rd.com/wp-content/uploads/2018/12/50-Funny-Animal-Pictures-That-You-Need-In-Your-Life-10.jpg"
+    "https://cdn.cnn.com/cnnnext/dam/assets/190913153747-comedy-wildlife-photography-awards-4.jpg",
+    "https://images.unsplash.com/photo-1497752531616-c3afd9760a11?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.businessinsider.com%2Ffunny-images-animal-comedy-wildlife-photography-awards-2015-12&psig=AOvVaw2a7L-RSvgqR93jT8bWSH4r&ust=1604934824044000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKibrtye8-wCFQAAAAAdAAAAABAD",
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Famazinganimalphotos.com%2Ffunny-animal-memes%2F78-funny-animals-pictures-with-captions-animal-photos-with-captions-7898.html&psig=AOvVaw2a7L-RSvgqR93jT8bWSH4r&ust=1604934824044000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKibrtye8-wCFQAAAAAdAAAAABAP"
 ];
