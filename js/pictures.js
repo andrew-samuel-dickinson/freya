9ec55b6e3f8e5b1bfac8d5c9e5b023fa1c903d08
andrew-samuel-dickinson <andrew.samuel.dickinson@gmail.com>
2020-12-19 13:38:11 +0000

    Updated pictures.js

--- js/pictures.js
+++ js/pictures.js
@@ -1,7 +1,7 @@
 images = 
 [
-    "https://cdn.cnn.com/cnnnext/dam/assets/190913153747-comedy-wildlife-photography-awards-4.jpg",
-    "https://images.unsplash.com/photo-1497752531616-c3afd9760a11?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
-    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.businessinsider.com%2Ffunny-images-animal-comedy-wildlife-photography-awards-2015-12&psig=AOvVaw2a7L-RSvgqR93jT8bWSH4r&ust=1604934824044000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKibrtye8-wCFQAAAAAdAAAAABAD",
-    "https://www.google.com/url?sa=i&url=https%3A%2F%2Famazinganimalphotos.com%2Ffunny-animal-memes%2F78-funny-animals-pictures-with-captions-animal-photos-with-captions-7898.html&psig=AOvVaw2a7L-RSvgqR93jT8bWSH4r&ust=1604934824044000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKibrtye8-wCFQAAAAAdAAAAABAP"
+    "https://hips.hearstapps.com/rbk.h-cdn.co/assets/17/35/1504106954-monkey.jpg?crop=1xw:1xh;center,top&resize=980:*",
+    "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-452291-001-1585074018.jpg?crop=1xw:1xh;center,top&resize=980:*",
+    "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-121809642-1-1585074238.jpg?crop=1xw:1xh;center,top&resize=980:*",
+    "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/chipmunk-nature-photos-1537973822.jpg?crop=1xw:1xh;center,top&resize=980:*"
 ];
